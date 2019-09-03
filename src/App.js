513e9935440bb5feaaa89c277ae6523a7ddca0d9
Ruiyu <ruiyuwu1998@gmail.com>
2019-09-03 17:44:44 -0400

    Clean up index.html file and create header, content, sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Header } from './components/layout/Header'
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
 
 export const App = () => (
   <div className="App">
     <header className="App-header">
-      <p><Header /></p>
+      <Header />
+      <Content />
     </header>
   </div>
 );
